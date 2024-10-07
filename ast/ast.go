3b02cb3d1a0ea8f32fd4f2f94b662f7505087c68
MichaelVittori <vittori.michael@gmail.com>
2024-10-07 10:03:40 -0400

    Made progress on AST construction - Added String methods for testing - Added ast_test to verify AST construction works

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "mockc/token"
+import (
+	"mockc/token"
+	"bytes"
+)
 
 type Node interface { // Define our treenode interface
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface { // Statement interface
@@ -28,6 +32,16 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements { // For each string, add that string to buffer
+		out.WriteString(s.String())
+	}
+
+	return out.String() // Return the string form of the buffer
+}
+
 type LetStatement struct {
 	Token token.Token // token.LET token
 	Name *Identifier
@@ -36,6 +50,20 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ") // Space after let
+	out.WriteString(ls.Name.String()) // Identifier
+	out.WriteString(" = ") // Add equals to buffer
+
+	if ls.Value != nil { // If the let statement has a second side, add to buffer
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+	return out.String() // Return the whole let statement as a string
+}
 
 type Identifier struct {
 	Token token.Token // Identifier token
@@ -44,6 +72,7 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string { return i.Value } // Helper method used by statement String methods to get name
 
 type ReturnStatement struct {
 	Token token.Token // Return token
@@ -52,3 +81,30 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ") // Space after return statement
+	if rs.ReturnValue != nil { // If there's a value being returned, add it to buffer
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";") // Add semicolon to end
+
+	return out.String() // Return as string
+}
+
+
+type ExpressionStatement struct {
+	Token token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+	if es.Expression == nil { // If there's no expression just return a blank string
+		return ""
+	} else { // Otherwise return the whole line
+		return es.Expression.String()
+	}
+}
---
+++ ast/ast_test.go
@@ -0,0 +1,28 @@
+package ast
+
+import (
+	"mockc/token"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{ // Constructing expected AST by hand
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENTIFIER, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENTIFIER, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() is wrong, got=%q", program.String())
+	}
+}

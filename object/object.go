4e7974c970d8263bc0ecfc06e597c314188aff38
MichaelVittori <vittori.michael@gmail.com>
2024-10-18 13:37:52 -0400

    Created object wrappers

--- object/object.go
+++ object/object.go
@@ -1,1 +1,37 @@
 package object
+
+import (
+	"fmt"
+)
+
+type ObjectType string
+const (
+	INTEGER_OBJECT = "INTEGER"
+	BOOLEAN_OBJECT = "BOOLEAN"
+	NULL_OBJECT = "NULL"
+)
+
+// All values encountered when evaluating Moxie source code will be wrapped in a struct fulfilling the Object interface
+type Object interface { // Note for myself: interfaces abstractly define behavior, structs define states/values
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJECT }
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJECT }
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value)}
+
+type Null struct {} // No value field because null has no value
+
+func (n *Null) Type() ObjectType { return NULL_OBJECT }
+func (n *Null) Inspect() string { return fmt.Sprintf("null")}
